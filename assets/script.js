39b1ec7fc6949a97b13c6d7760f2ee66c761e44c
Robert Anderson <reanderson89@gmail.com>
2020-10-11 12:40:43 -0400

    added bootstrap links and tested API to verify connection.

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,35 @@
-console.log("Hello World");
+$(document).ready(function(){
+
+var city = "Atlanta"
+var apiKey = "07fc03bb700aad88a938de39fb56c70a"
+var queryUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+apiKey+"&units=imperial";
+
+
+
+$.ajax({
+    url: queryUrl,
+    method: "GET"
+}).then(function(response){
+    console.log(response);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
